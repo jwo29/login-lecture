4cc0af214a0836ce531ef77f0960bf793b36e285
Lee-JiWoo <jwo2955@gmail.com>
2022-02-27 13:42:37 +0900

    stored HTML objects by using DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,20 @@
 "use strict";
 
-console.log("hello-login.js");
-console.log("bye-login.js");
+// 로그인 기능 구현
+
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+
+    console.log(req);
+    // 서버에 전달
+    // fetch();
+}
