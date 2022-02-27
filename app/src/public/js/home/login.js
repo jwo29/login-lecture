123af9d02808c4432bcd05c57b218ff2f8a2a203
Lee-JiWoo <jwo2955@gmail.com>
2022-02-27 13:48:50 +0900

    created fetch() requesting login

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -15,6 +15,14 @@ function login() {
     };
 
     console.log(req);
+    console.log(JSON.stringify(req));
+
     // 서버에 전달
-    // fetch();
+    fetch("/login", { // /login 경로에 전달
+        method: "POST", // /login 경로로 POST 메서드로 접근할 수 있는 서버 API가 있어야 함
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req), // 오브젝트를 JSON으로 변경하여 서버에 전달 
+    });
 }
