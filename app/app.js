0754b03cf308bb6c58e182cce2efa13266250e77
Lee-JiWoo <jwo2955@gmail.com>
2022-02-27 13:06:37 +0900

    craeted javascript file on front-end

--- app/app.js
+++ app/app.js
@@ -12,6 +12,6 @@ app.set("views", "./src/views"); // 뷰 엔진 세팅. views를 관리해줄 폴
 app.set("view engine", "ejs"); // html 파일을 ejsd 뷰 엔진으로 해석할 것임
 
 app.use("/", home) // use() - 미들웨어를 등록하는 메서드
-
+app.use(express.static(`${__dirname}/src/public`))
 
 module.exports = app;
